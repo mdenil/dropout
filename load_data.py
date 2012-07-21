e4985334063dcf1617cb6364ef5538ee8de725ad
Misha Denil <misha.denil@gmail.com>
2012-07-21 09:42:58 -0700

    A function to load my own version of mnist.

--- load_data.py
+++ load_data.py
@@ -7,7 +7,46 @@ import sys
 import theano
 import theano.tensor as T
 
-def load_data(dataset):
+def _shared_dataset(data_xy):
+    """ Function that loads the dataset into shared variables
+
+    The reason we store our dataset in shared variables is to allow
+    Theano to copy it into the GPU memory (when code is run on GPU).
+    Since copying data into the GPU is slow, copying a minibatch everytime
+    is needed (the default behaviour if the data is not in a shared
+    variable) would lead to a large decrease in performance.
+    """
+    data_x, data_y = data_xy
+    shared_x = theano.shared(np.asarray(data_x,
+                                           dtype=theano.config.floatX))
+    shared_y = theano.shared(np.asarray(data_y,
+                                           dtype=theano.config.floatX))
+    # When storing data on the GPU it has to be stored as floats
+    # therefore we will store the labels as ``floatX`` as well
+    # (``shared_y`` does exactly that). But during our computations
+    # we need them as ints (we use labels as index, and if they are
+    # floats it doesn't make sense) therefore instead of returning
+    # ``shared_y`` we will have to cast it to int. This little hack
+    # lets ous get around this issue
+    return shared_x, T.cast(shared_y, 'int32')
+
+def load_mnist(path):
+    mnist = np.load(path)
+    train_set_x = mnist['train_data']
+    train_set_y = mnist['train_labels']
+    test_set_x = mnist['test_data']
+    test_set_y = mnist['test_labels']
+
+    train_set_x, train_set_y = _shared_dataset((train_set_x, train_set_y))
+    test_set_x, test_set_y = _shared_dataset((test_set_x, test_set_y))
+    valid_set_x, valid_set_y = test_set_x, test_set_y
+    
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+            (test_set_x, test_set_y)]
+    return rval
+
+
+def load_umontreal_data(dataset):
     ''' Loads the dataset
 
     :type dataset: string
@@ -39,7 +78,7 @@ def load_data(dataset):
     #the number of rows in the input. It should give the target
     #target to the example with the same index in the input.
 
-    def shared_dataset(data_xy):
+    def _shared_dataset(data_xy):
         """ Function that loads the dataset into shared variables
 
         The reason we store our dataset in shared variables is to allow
@@ -62,9 +101,9 @@ def load_data(dataset):
         # lets ous get around this issue
         return shared_x, T.cast(shared_y, 'int32')
 
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
+    test_set_x, test_set_y = _shared_dataset(test_set)
+    valid_set_x, valid_set_y = _shared_dataset(valid_set)
+    train_set_x, train_set_y = _shared_dataset(train_set)
 
     rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
             (test_set_x, test_set_y)]
