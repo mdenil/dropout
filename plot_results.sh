b0743ed48596e8c12c07f458969d3b71be3adee5
Misha Denil <misha.denil@gmail.com>
2012-11-13 05:38:58 -0800

    Add moving_average.
    This was referenced in plot_results.sh but not included.

--- plot_results.sh
+++ plot_results.sh
@@ -18,15 +18,15 @@ set output "$PLOT_FILE"
 set title "Number of errors on MNIST test set."
 set xlabel "epoch"
 set ylabel "num errors"
-set xrange [1:3000]
+set xrange [10:3000]
 #set yrange [100:10000]
 #set yrange [80:1000]
 #set yrange [80:500]
-set yrange [80:220]
+#set yrange [80:220]
 #set logscale y
-#set logscale x
-plot "<cat results_backprop.txt | moving_average $SMOOTHING" title "backprop", \
-     "<cat results_dropout.txt | moving_average $SMOOTHING"  title "dropout"
+set logscale x
+plot "<cat results_backprop.txt | bin/moving_average $SMOOTHING" title "backprop", \
+     "<cat results_dropout.txt | bin/moving_average $SMOOTHING"  title "dropout"
 
 EOF
 ) 
